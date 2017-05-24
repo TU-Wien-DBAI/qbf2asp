6e2c19239c796121af726d6b63b6ce2a7aaaef42
Jannik Vierling <jannik.vierling@gmail.com>
2017-05-24 07:09:26 +0000

    added first implementation of 3-QBF reduction

--- src/algorithms/LargeRuleIclp2016Rewriter.cpp
+++ src/algorithms/LargeRuleIclp2016Rewriter.cpp
@@ -23,11 +23,11 @@ namespace qbf2asp
   {
     rewrite(instance, std::cout);
   }
-
-
+  
   std::unordered_set<logic::variable_t> e_0_variables(const IQbfInstance & instance);
   std::unordered_set<logic::variable_t> a_1_variables(const IQbfInstance & instance);
   std::unordered_set<logic::variable_t> e_2_variables(const IQbfInstance & instance);
+  const unordered_set<variable_t> x_variables(const IQbfInstance & instance);
   const std::vector<logic::variable_t> clause_variables(const IQbfClause & clause, const std::unordered_set<logic::variable_t> x_variables);
   bool plus_one(std::vector<bool> & list, unsigned int position);
   std::vector<bool> dual_tuple(const IQbfClause & clause, const std::vector<logic::variable_t> vars);
@@ -35,7 +35,19 @@ namespace qbf2asp
   void print_guess(ostream & out, const IQbfInstance & instance);
   void print_clause_facts(ostream & out, const IQbfClause & clause, int clause_id);
   void print_clause_rules(ostream & out, const IQbfClause & clause, int clause_id);
-  void print_constraint(ostream & out, const IQbfInstance & instance);
+  void print_constraint(ostream & out, const IQbfInstance & instance, bool qbf3sat);
+  void print_saturation_rules(ostream & out, const IQbfInstance & instance);
+  void print_saturation_constraint(ostream & out);
+
+  static const char * ASP_BOT = "";
+  static const char * ASP_VAR_Y = "Y";
+  static const char * ASP_PRED_C = "c";
+  static const char * ASP_PRED_T = "t";
+  static const char * ASP_PRED_F = "f";
+  static const char * ASP_TERM_X = "x";
+  static const char * ASP_PRED_SAT = "sat";
+  static const char * ASP_NOT      = "not";
+  static const char * ASP_RULE_SYM = ":-";
   
   void LargeRuleIclp2016Rewriter::rewrite(const IQbfInstance &instance, ostream &out) const
   {
@@ -43,8 +55,8 @@ namespace qbf2asp
       std::cerr << "Disjunctive QBF not supported at the moment" << endl;
       exit(4);
     }
-    if (instance.outermostQuantifierLevel() > 1) {
-      std::cerr << "Only two quantifier levels are supported right now" << endl;
+    if (instance.innermostQuantifierLevel() > 2) {
+      std::cerr << "Only three quantifier levels are supported" << endl;
       exit(4);
     }
 
@@ -56,12 +68,48 @@ namespace qbf2asp
       print_clause_facts(out, *itc, clause_id);
     }
     
-    print_constraint(out, instance);
+    print_constraint(out, instance, instance.innermostQuantifierLevel() == 2);
+
+    if (instance.innermostQuantifierLevel() == 2) {
+      print_saturation_constraint(out);
+      print_saturation_rules(out, instance);
+    }
   }
 
+  void print_saturation_rules(ostream & out, const IQbfInstance & instance)
+  {
+    const std::unordered_set<logic::variable_t> & forall_1_variables = a_1_variables(instance);
+    
+    for (auto it = forall_1_variables.begin(); it != forall_1_variables.end(); ++it) {
+      out << ASP_PRED_T << "(" << ASP_TERM_X << "_" << *it << ")"
+	  << " " << ASP_RULE_SYM << " " << ASP_PRED_SAT << "." << endl;
+      out << ASP_PRED_F << "(" << ASP_TERM_X << "_" << *it << ")"
+	  << " " << ASP_RULE_SYM << " " << ASP_PRED_SAT << "." << endl;
+    }
+    for (IQbfInstance::const_iterator itc = instance.begin(); itc != instance.end(); ++itc) {
+      int clause_id = std::distance(instance.begin(), itc);
+      vector<variable_t> clause_y = clause_variables(*itc, e_0_variables(instance));
+      std::vector<bool> tuple = std::vector<bool>(clause_y.size());
+      do {
+	out << ASP_PRED_C << "_" << clause_id;
+	if (clause_y.size() > 0) {
+	  out << "(";
+	  print_tuple(out, tuple);
+	  out << ")";
+	}
+	out << " " << ASP_RULE_SYM << " " << ASP_PRED_SAT << "." << endl;
+      } while (!plus_one(tuple, 0));
+    }
+  }
+  
+  void print_saturation_constraint(ostream & out)
+  {
+    out << ASP_BOT << " " << ASP_RULE_SYM << " " << ASP_NOT << " " << ASP_PRED_SAT << "." << endl;
+  }
+  
   void print_clause_rules(ostream & out, const IQbfClause & clause, int clause_id)
   {
-    std::vector<logic::variable_t> clause_x = clause_variables(clause, a_1_variables(clause.instance()));
+    std::vector<logic::variable_t> clause_x = clause_variables(clause, x_variables(clause.instance()));
     std::vector<logic::variable_t> clause_y = clause_variables(clause, e_0_variables(clause.instance()));
     for (auto it = clause_x.begin(); it != clause_x.end(); ++it) {
       std::vector<bool> tuple = std::vector<bool>(clause_y.size());
@@ -95,9 +143,9 @@ namespace qbf2asp
 
   void print_guess(ostream & out, const IQbfInstance & instance)
   {
-    const std::unordered_set<logic::variable_t> & forall_1_variables = a_1_variables(instance);
+    const std::unordered_set<logic::variable_t> x_vars = x_variables(instance);
     
-    for (auto it = forall_1_variables.begin(); it != forall_1_variables.end(); ++it) {
+    for (auto it = x_vars.begin(); it != x_vars.end(); ++it) {
       out << "t(x_" << *it << ") | f(x_" << *it << ")." << endl;
     }
   }
@@ -106,14 +154,14 @@ namespace qbf2asp
   {
     std::vector<logic::variable_t> clause_y_vars = clause_variables(clause, e_0_variables(clause.instance()));
     for (unsigned int i = 0; i < clause_y_vars.size(); i++) {
-      out << (i != 0 ? "," : "") << "Y_" << clause_y_vars.at(i);
+      out << (i != 0 ? "," : "") << ASP_VAR_Y << "_" << clause_y_vars.at(i);
     }
   }
 
-  void print_constraint(ostream & out, const IQbfInstance & instance)
+  void print_constraint(ostream & out, const IQbfInstance & instance, bool qbf3sat)
   {
     bool first = true;
-    out << ":- ";
+    out << (qbf3sat ? ASP_PRED_SAT : ASP_BOT) << " :- ";
     for (IQbfInstance::const_iterator itc = instance.begin(); itc != instance.end(); ++itc) {
       int clause_id = std::distance(instance.begin(), itc);
       vector<variable_t> clause_y = clause_variables(*itc, e_0_variables(instance));
@@ -138,21 +186,29 @@ namespace qbf2asp
 
   std::unordered_set<logic::variable_t> e_0_variables(const IQbfInstance & instance)
   {
-    return instance.variables(instance.outermostQuantifierLevel());
+    return instance.variables(instance.innermostQuantifierLevel());
   }
 
   std::unordered_set<logic::variable_t> a_1_variables(const IQbfInstance & instance)
   {
-    return instance.outermostQuantifierLevel() - 1 >= 0
-      ? instance.variables(instance.outermostQuantifierLevel() - 1)
+    return instance.innermostQuantifierLevel() - 1 >= 0
+      ? instance.variables(instance.innermostQuantifierLevel() - 1)
       : std::unordered_set<logic::variable_t>();
   }
 
   std::unordered_set<logic::variable_t> e_2_variables(const IQbfInstance & instance)
   {
-    return instance.outermostQuantifierLevel() - 2 >= 0
-      ? instance.variables(instance.outermostQuantifierLevel() - 2)
+    return instance.innermostQuantifierLevel() - 2 >= 0
+      ? instance.variables(instance.innermostQuantifierLevel() - 2)
       : std::unordered_set<logic::variable_t>();
+  }
+
+  const unordered_set<variable_t> x_variables(const IQbfInstance & instance)
+  {
+    const unordered_set<variable_t> a_vars = a_1_variables(instance);
+    unordered_set<variable_t> x_vars = unordered_set<variable_t>(e_2_variables(instance));
+    x_vars.insert(a_vars.begin(), a_vars.end());
+    return x_vars;
   }
   
   bool plus_one(std::vector<bool> & list, unsigned int position)
